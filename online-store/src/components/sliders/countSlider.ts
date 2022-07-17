9005244be4f31e0d3dbadb5262e3be92470b05fd
Kiryl Knyazev <kiril.kn99@email.ru>
2022-07-18 00:05:35 +0300

    refactor: countSlider.ts and yearSlider.ts files

--- online-store/src/components/sliders/countSlider.ts
+++ online-store/src/components/sliders/countSlider.ts
@@ -2,24 +2,24 @@ import * as noUiSlider from 'nouislider';
 import Filter from '../filters/filter';
 import { cards } from '../../data/cards';
 
-const sliderCount: noUiSlider.target = document.querySelector('.range-count .slider') as noUiSlider.target;
-const minCountValue = document.querySelector('.range-count .min-value') as HTMLElement;
-const maxCountValue = document.querySelector('.range-count .max-value') as HTMLElement;
-
 class CountSlider {
+    public static sliderCount: noUiSlider.target = document.querySelector('.range-count .slider') as noUiSlider.target;
+    public static minCountValue = document.querySelector('.range-count .min-value') as HTMLElement;
+    public static maxCountValue = document.querySelector('.range-count .max-value') as HTMLElement;
+
     public createCountSlider(): void {
-        if (sliderCount) {
-            noUiSlider.create(sliderCount, {
+        if (CountSlider.sliderCount) {
+            noUiSlider.create(CountSlider.sliderCount, {
                 start: [
-                    localStorage.getItem('rangeCountMin') || minCountValue.innerHTML,
-                    localStorage.getItem('rangeCountMax') || maxCountValue.innerHTML,
+                    localStorage.getItem('rangeCountMin') || CountSlider.minCountValue.innerHTML,
+                    localStorage.getItem('rangeCountMax') || CountSlider.maxCountValue.innerHTML,
                 ],
                 tooltips: true,
                 step: 1,
                 connect: true,
                 range: {
-                    min: Number(minCountValue.innerHTML),
-                    max: Number(maxCountValue.innerHTML),
+                    min: Number(CountSlider.minCountValue.innerHTML),
+                    max: Number(CountSlider.maxCountValue.innerHTML),
                 },
                 format: {
                     to: function (value) {
@@ -32,12 +32,12 @@ class CountSlider {
                 },
             });
 
-            if (sliderCount.noUiSlider) {
-                const values: HTMLElement[] = [minCountValue, maxCountValue];
-                sliderCount.noUiSlider.on('update', function (items, handle) {
+            if (CountSlider.sliderCount.noUiSlider) {
+                const values: HTMLElement[] = [CountSlider.minCountValue, CountSlider.maxCountValue];
+                CountSlider.sliderCount.noUiSlider.on('update', function (items, handle) {
                     values[handle].innerHTML = Math.round(Number(items[handle])).toString();
-                    localStorage.setItem('rangeCountMin', minCountValue.innerHTML);
-                    localStorage.setItem('rangeCountMax', maxCountValue.innerHTML);
+                    localStorage.setItem('rangeCountMin', CountSlider.minCountValue.innerHTML);
+                    localStorage.setItem('rangeCountMax', CountSlider.maxCountValue.innerHTML);
                     new Filter().filterCards(cards);
                 });
             }
--- online-store/src/components/sliders/yearSlider.ts
+++ online-store/src/components/sliders/yearSlider.ts
@@ -2,24 +2,24 @@ import * as noUiSlider from 'nouislider';
 import Filter from '../filters/filter';
 import { cards } from '../../data/cards';
 
-const minYearValue = document.querySelector('.range-year .min-value') as HTMLElement;
-const maxYearValue = document.querySelector('.range-year .max-value') as HTMLElement;
-const sliderYear: noUiSlider.target = document.querySelector('.range-year .slider') as noUiSlider.target;
-
 class YearSlider {
+    public static sliderYear: noUiSlider.target = document.querySelector('.range-year .slider') as noUiSlider.target;
+    public static minYearValue = document.querySelector('.range-year .min-value') as HTMLElement;
+    public static maxYearValue = document.querySelector('.range-year .max-value') as HTMLElement;
+
     public createYearSlider(): void {
-        if (sliderYear) {
-            noUiSlider.create(sliderYear, {
+        if (YearSlider.sliderYear) {
+            noUiSlider.create(YearSlider.sliderYear, {
                 start: [
-                    localStorage.getItem('rangeYearMin') || minYearValue.innerHTML,
-                    localStorage.getItem('rangeYearMax') || maxYearValue.innerHTML,
+                    localStorage.getItem('rangeYearMin') || YearSlider.minYearValue.innerHTML,
+                    localStorage.getItem('rangeYearMax') || YearSlider.maxYearValue.innerHTML,
                 ],
                 tooltips: true,
                 step: 1,
                 connect: true,
                 range: {
-                    min: Number(minYearValue.innerHTML),
-                    max: Number(maxYearValue.innerHTML),
+                    min: Number(YearSlider.minYearValue.innerHTML),
+                    max: Number(YearSlider.maxYearValue.innerHTML),
                 },
                 format: {
                     to: function (value) {
@@ -32,12 +32,12 @@ class YearSlider {
                 },
             });
 
-            if (sliderYear.noUiSlider) {
-                const values: HTMLElement[] = [minYearValue, maxYearValue];
-                sliderYear.noUiSlider.on('update', function (items, handle) {
+            if (YearSlider.sliderYear.noUiSlider) {
+                const values: HTMLElement[] = [YearSlider.minYearValue, YearSlider.maxYearValue];
+                YearSlider.sliderYear.noUiSlider.on('update', function (items, handle) {
                     values[handle].innerHTML = Math.round(Number(items[handle])).toString();
-                    localStorage.setItem('rangeYearMin', minYearValue.innerHTML);
-                    localStorage.setItem('rangeYearMax', maxYearValue.innerHTML);
+                    localStorage.setItem('rangeYearMin', YearSlider.minYearValue.innerHTML);
+                    localStorage.setItem('rangeYearMax', YearSlider.maxYearValue.innerHTML);
                     new Filter().filterCards(cards);
                 });
             }
